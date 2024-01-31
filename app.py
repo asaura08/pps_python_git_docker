b930233edbbad085216273fbd95d98311abe8fb5
root <root@Elnegren-pc.>
2024-01-31 15:55:03 +0100

    Hola mundo en web

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from email.policy import default
+from urllib import response
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Ruta de la página principal
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    html_content = """
+    <html>
+    <head>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-size: 2rem;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+    </body>
+    </html>
+    """
+    return html_content
